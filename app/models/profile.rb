fb37750151593e9757fb0fd499ea21a23d90eea6
kawabata324 <kawabataharuki48@gmail.com>
2023-03-10 23:11:52 +0900

    :art: validateの修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,10 @@
 class Profile < ApplicationRecord
+  belongs_to :personal_history
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   PHONE_NUMBER_LENGTH = 11
 
-  validates :email, { format: { with: VALID_EMAIL_REGEX } }
-  validates :phone_number, { length: PHONE_NUMBER_LENGTH }
-  validates :birth_date_on, comparison: { less_than: Proc.new { Date.today } }
+  validates :email, { format: { with: VALID_EMAIL_REGEX }, allow_blank: true }
+  validates :phone_number, { length: { is: PHONE_NUMBER_LENGTH }, allow_blank: true }
+  validates :birth_date_on, { comparison: { less_than: Proc.new { Date.today } }, allow_blank: true }
 end
