10c8cbfd02d0087bc254aa314e31a67041515eb6
kawabata324 <kawabataharuki48@gmail.com>
2023-03-11 16:54:38 +0900

    :art: chain address and personal_history

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,10 @@ class Address < ApplicationRecord
   belongs_to :personal_history
 
   # TODO: あとでモデルのバリデーションかく
+
+  def format_postal_code
+    return '' if postal_code.nil?
+
+    "#{postal_code[0..2] || ''}-#{postal_code[3..6] || ''}"
+  end
 end
--- app/models/personal_history.rb
+++ app/models/personal_history.rb
@@ -1,5 +1,6 @@
 class PersonalHistory < ApplicationRecord
   has_one :profile
+  has_one :address
 
   def updated_on
     self.updated_at.to_date
